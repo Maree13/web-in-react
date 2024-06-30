11a2bbf83cfbc301fba05b20589dd69012329d54
Mariana Sarovic <sarovicmariana@gmail.com>
2024-07-01 00:17:11 +0200

    css About me

--- src/data.js
+++ src/data.js
@@ -7,28 +7,28 @@ const allCards = [
   {
     id: 1,
     image: image1,
-    title: 'Title 1',
+    title: 'Parallel',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque luctus dolor vitae ex ultrices.',
   },
   {
     id: 2,
     image: image2,
-    title: 'Title 2',
+    title: 'Space',
     description:
       'Aenean euismod, turpis eget ultricies tincidunt, arcu lorem pellentesque eros, non tempor orci.',
   },
   {
     id: 3,
     image: image3,
-    title: 'Title 3',
+    title: 'Plastic',
     description:
       'Sed quis lacus in velit ultrices pretium. Curabitur ac sem sit amet urna hendrerit condimentum.',
   },
   {
     id: 4,
     image: image4,
-    title: 'Title 4',
+    title: 'Dream',
     description:
       'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem accusantium amet nemo minus quo architecto quas',
   },
@@ -37,25 +37,25 @@ const allCards = [
 const tabs = [
   {
     id: 0,
-    title: 'Tab 1',
+    title: 'Parallel',
     content:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
   },
   {
     id: 1,
-    title: 'Tab 2',
+    title: 'Space',
     content:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero.',
   },
   {
     id: 2,
-    title: 'Tab 3',
+    title: 'Plastic',
     content:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus imperdiet, nulla et dictum interdum, nisi lorem egestas odio, vitae scelerisque enim ligula venenatis dolor. Maecenas nisl est, ultrices nec congue eget, auctor vitae massa. Fusce luctus vestibulum augue ut aliquet. Mauris ante ligula, facilisis sed ornare eu, lobortis in odio. Praesent convallis urna a lacus interdum ut hendrerit risus congue. Nunc sagittis dictum nisi.',
   },
   {
     id: 3,
-    title: 'Tab 4',
+    title: 'Dream',
     content:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat.',
   },
